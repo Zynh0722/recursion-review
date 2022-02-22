9edee4aa8a1d522ff7bf34e51ec2815130ae1c46
Zynh Ludwig <Zynh0722@gmail.com>
2022-02-22 12:08:56 -0800

    Fix Test and Write getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var matchingElements = [];
+
+  var checkChildren = function(node) {
+    // Take node, check if class list of that node contains class list.
+    // If it does contain the class list:
+    if (_.contains(node.classList, className)) {
+      matchingElements.push(node);
+    }
+
+    // Loop through all the children and check the children by
+    // calling the same recursive function
+    _.each(node.childNodes, function(child) {
+      checkChildren(child);
+    });
+
+  };
+
+  checkChildren(document.body);
+
+  return matchingElements;
+
 };
